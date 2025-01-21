2dfa2709231c7db16027b3144fb8939063bbafc9
Ben McGraw <>
2025-01-21 00:46:09 -0800

    Add flippable cards

--- frontend/src/pages/BoardPage.tsx
+++ frontend/src/pages/BoardPage.tsx
@@ -1,4 +1,6 @@
+import React, { useState } from 'react';
 
+import styles from '../components/Card.module.css'
 import Card, { type PokerCard } from '../components/Card';
 
 function BoardPage() {
@@ -7,16 +9,50 @@ function BoardPage() {
   const card_2: PokerCard = { suit: '♣', rank: 'Q' };
   const card_3: PokerCard = { suit: 'H', rank: '4' };
 
+  type CardState = [PokerCard, boolean];
+
+  const cardState: CardState[] = [
+    [card_1, true],
+    [card_2, false],
+    [card_3, true],
+  ];
+
+  // usestate to keep track of the cards. clicking on them changes the boolean
+  // if the card is face up or face down
+
+  const [cards, setCards] = useState(cardState);
+
   return (
     <div className="BoardPage">
       <header className="App-header">
         Board Game
-        <Card card={card_1} faceUp={true} />
-        <Card card={card_2} faceUp={false} />
-        <Card card={card_3} faceUp={true} />
+
+
+
+        <div style={{ background: 'green', width: '90%', height: '300px', margin: 'auto', paddingTop: '40px', borderRadius: '10px' }} >
+          {cards.map(([card, faceUp]) => (
+            ClickableCard(setCards, card, faceUp)
+          ))}
+        </div>
+
       </header>
     </div>
   );
 }
 
 export default BoardPage;
+
+function ClickableCard(setCards: React.Dispatch<React.SetStateAction<[PokerCard, boolean][]>>, card: PokerCard, faceUp: boolean) {
+
+  const style = {
+    background: 'none',
+    border: 'none'
+  }
+
+  return <button style={style} onClick={() => {
+    setCards(prevCards => prevCards.map(([c, faceUp]) => c === card ? [c, !faceUp] : [c, faceUp]
+    ));
+  }}>
+    <Card card={card} faceUp={faceUp} />
+  </button>;
+}
