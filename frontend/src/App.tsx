56a31835a0f84c738c18e3cd0539f3b8e5faff86
Ben McGraw <>
2025-01-20 19:34:42 -0800

    Add a router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,3 +24,16 @@ function App() {
 }
 
 export default App;
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router";
+// import App from "./app";
+
+// const root = document.getElementById("root");
+
+// ReactDOM.createRoot(root).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
