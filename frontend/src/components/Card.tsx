284238ef8d9bc007cfbe05ecf007d67e2861f9e7
Ben McGraw <>
2025-01-20 21:02:58 -0800

    Add a card to abouit page

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,12 +1,20 @@
 
 import React from 'react';
 import styles from './Card.module.css';
+// @ts-ignore
+import { Sq } from '@letele/playing-cards/dist/index.esm.js'
+
+// console.log(deck)
+
+const mult = 30;
+
+const width = 5 * mult;
+const height = 7 * mult;
 
 function Card() {
   return (
-    <div className={styles.card}>
-      <div className={styles.topLeft}>A</div>
-      <div className={styles.bottomRight}>A</div>
+    <div style={{ backgroundColor: 'green' }}>
+      <Sq style={{ height: `${height}px`, width: `${width}px` }} />
     </div>
   );
 }
