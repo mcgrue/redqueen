fcb709ae871e8e81f921f0449e16e8724c844148
Ben McGraw <>
2025-01-21 00:34:45 -0800

    display 3 cards

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import styles from './Card.module.css';
 // @ts-ignore
-import { Sq } from '@letele/playing-cards/dist/index.esm.js'
+import * as deck from '@letele/playing-cards/dist/index.esm.js'
 
 // console.log(deck)
 
@@ -11,12 +11,74 @@ const mult = 30;
 const width = 5 * mult;
 const height = 7 * mult;
 
-function Card() {
+const symbolToLetter = (suit: PokerSuit) => {
+  switch (suit) {
+    case '♠':
+      return 'S';
+    case '♣':
+      return 'C';
+    case '♥':
+      return 'H';
+    case '♦':
+      return 'D';
+  }
+
+  return suit;
+}
+
+const toLeteleIdx = (card: PokerCard): LeteleIdx => {
+  const suits = ['S', 'C', 'H', 'D'];
+  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  const _1 = symbolToLetter(card.suit);
+  const _2 = card.rank.toLowerCase();
+
+  return `${_1}${_2}` as LeteleIdx;
+}
+
+/**
+ The package provides SVG components for standard playing cards, including different ranks (A, 2-10, J, Q, K) and suits (Hearts, Diamonds, Clubs, Spades). Additionaly there are 2 joker cards (J1, J2) and 2 back side cards (B1,B2) that represent the backside of a card.
+Card Naming Convention
+
+The naming convention for card components is a combination of the suit and rank initials.
+
+    H:Hearts,D:Diamonds,C:Clubs,S:Spades
+    The ranks are: (a,2-10,j,q,k)
+    For instance, H2 represents the 2 of Hearts, and Sk represents the King of Spades.
+ */
+type LeteleIdx = 'Ha' | 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6' | 'H7' | 'H8' | 'H9' | 'H10' | 'Hj' | 'Hq' | 'Hk' | 'Da' | 'D1' | 'D2' | 'D3' | 'D4' | 'D5' | 'D6' | 'D7' | 'D8' | 'D9' | 'D10' | 'Dj' | 'Dq' | 'Dk' | 'Ca' | 'C1' | 'C2' | 'C3' | 'C4' | 'C5' | 'C6' | 'C7' | 'C8' | 'C9' | 'C10' | 'Cj' | 'Cq' | 'Ck' | 'Sa' | 'S1' | 'S2' | 'S3' | 'S4' | 'S5' | 'S6' | 'S7' | 'S8' | 'S9' | 'S10' | 'Sj' | 'Sq' | 'Sk' | 'B1' | 'B2' | 'J1' | 'J2';
+
+interface CardProps {
+  card: PokerCard;
+  faceUp: boolean;
+}
+
+const Card: React.FC<CardProps> = ({ card, faceUp }) => {
+  const idx = faceUp ? toLeteleIdx(card) : 'B2';
+
+  const Card = deck[idx];
+
+  let extraProps: any = {};
+  if (faceUp) {
+    extraProps['data-rank'] = card.rank;
+    extraProps['data-suit'] = card.suit;
+    extraProps['data-faceup'] = 'true';
+  } else {
+    extraProps['data-faceup'] = 'false';
+  }
+
   return (
-    <div style={{ backgroundColor: 'green' }}>
-      <Sq style={{ height: `${height}px`, width: `${width}px` }} />
+    <div style={{ backgroundColor: 'green' }} className="PokerCard" {...extraProps}>
+      <Card style={{ height: `${height}px`, width: `${width}px` }} />
     </div>
   );
 }
+
+export type PokerSuit = '♠' | '♣' | '♥' | '♦' | 'S' | 'C' | 'H' | 'D';
+export type PokerRank = 'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K';
+export type PokerCard = {
+  suit: PokerSuit;
+  rank: PokerRank;
+};
 
 export default Card;
--- frontend/src/pages/BoardPage.tsx
+++ frontend/src/pages/BoardPage.tsx
@@ -1,12 +1,19 @@
 
-import Card from '../components/Card';
+import Card, { type PokerCard } from '../components/Card';
 
 function BoardPage() {
+
+  const card_1: PokerCard = { suit: 'D', rank: '10' };
+  const card_2: PokerCard = { suit: '♣', rank: 'Q' };
+  const card_3: PokerCard = { suit: 'H', rank: '4' };
+
   return (
     <div className="BoardPage">
       <header className="App-header">
         Board Game
-        <Card />
+        <Card card={card_1} faceUp={true} />
+        <Card card={card_2} faceUp={false} />
+        <Card card={card_3} faceUp={true} />
       </header>
     </div>
   );
